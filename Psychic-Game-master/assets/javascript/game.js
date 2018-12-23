8b48b629c5cb69b3249ec0f4e1da69dc50d8a758
timdbeirne <45347627+timdbeirne@users.noreply.github.com>
2018-12-22 22:12:09 -0600

    Add files via upload

---
+++ Psychic-Game-master/assets/javascript/game.js
@@ -0,0 +1,9 @@
+var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+var wins(function())
+var losses(function())
+var guesses(function())
+
+ for (i = 0; i < letters.length; i++) 
+
+    
